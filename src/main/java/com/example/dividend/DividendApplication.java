9fd931c04a98ee74cf12c9e3ac2783f6fb680169
Cheol Lee <chullee49@naver.com>
2022-12-03 18:58:15 +0900

    fourth commit

--- src/main/java/com/example/dividend/DividendApplication.java
+++ src/main/java/com/example/dividend/DividendApplication.java
@@ -4,8 +4,10 @@ import org.apache.commons.collections4.Trie;
 import org.apache.commons.collections4.trie.PatriciaTrie;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class DividendApplication {
 
     public static void main(String[] args) {
