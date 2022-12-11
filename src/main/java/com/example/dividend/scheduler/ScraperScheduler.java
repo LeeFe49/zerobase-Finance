e613b922ed38d5551cede454bc99c4554eb9df6e
Cheol Lee <chullee49@naver.com>
2022-12-11 18:34:55 +0900

    seventh commit

--- src/main/java/com/example/dividend/scheduler/ScraperScheduler.java
+++ src/main/java/com/example/dividend/scheduler/ScraperScheduler.java
@@ -2,6 +2,7 @@ package com.example.dividend.scheduler;
 
 import com.example.dividend.model.Company;
 import com.example.dividend.model.ScrapedResult;
+import com.example.dividend.model.constants.CacheKey;
 import com.example.dividend.persist.CompanyRepository;
 import com.example.dividend.persist.DividendRepository;
 import com.example.dividend.persist.entity.CompanyEntity;
@@ -9,6 +10,8 @@ import com.example.dividend.persist.entity.DividendEntity;
 import com.example.dividend.scraper.Scraper;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -17,6 +20,7 @@ import java.util.List;
 
 @Slf4j
 @Component
+@EnableCaching
 @AllArgsConstructor
 public class ScraperScheduler {
 
@@ -25,19 +29,9 @@ public class ScraperScheduler {
 
     private final Scraper yahooFinanceScraper;
 
-    @Scheduled(fixedDelay = 1000)
-    public void test1() throws InterruptedException {
-        Thread.sleep(10000);
-        System.out.println(Thread.currentThread().getName() + "테스트 1 : " + LocalDateTime.now());
-    }
-
-    @Scheduled(fixedDelay = 1000)
-    public void test2(){
-        System.out.println(Thread.currentThread().getName() + "테스트 2 : " + LocalDateTime.now());
-    }
-
     //일정 주기마다 수행
-//    @Scheduled(cron = "${scheduler.scrap.yahoo}")
+    @CacheEvict(value = CacheKey.KEY_FINANCE, allEntries = true)
+    @Scheduled(cron = "${scheduler.scrap.yahoo}")
     public void yahooFinanceScheduling() {
         log.info("scraping scheduler is started");
         // 지정된 회사 목록을 조회
@@ -46,10 +40,9 @@ public class ScraperScheduler {
         // 회사마다 배당금 정보를 새로 스크래핑
         for (var company : companies) {
             log.info("scraping scheduler is started -> " + company.getName());
-            ScrapedResult scrapedResult = this.yahooFinanceScraper.scrap(Company.builder()
-                    .name(company.getName())
-                    .ticker(company.getTicker())
-                    .build());
+            ScrapedResult scrapedResult = this.yahooFinanceScraper.scrap(
+                    new Company(company.getTicker(), company.getName()));
+
             // 스크래핑한 배당금 정보 중 데이터베이스에 없는 값은 저장
             scrapedResult.getDividends().stream()
                     // 디비든 모델을 디비든 엔티티로 매핑
--- src/main/java/com/example/dividend/service/FinanceService.java
+++ src/main/java/com/example/dividend/service/FinanceService.java
@@ -3,11 +3,14 @@ package com.example.dividend.service;
 import com.example.dividend.model.Company;
 import com.example.dividend.model.Dividend;
 import com.example.dividend.model.ScrapedResult;
+import com.example.dividend.model.constants.CacheKey;
 import com.example.dividend.persist.CompanyRepository;
 import com.example.dividend.persist.DividendRepository;
 import com.example.dividend.persist.entity.CompanyEntity;
 import com.example.dividend.persist.entity.DividendEntity;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -15,6 +18,7 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
+@Slf4j
 @Service
 @AllArgsConstructor
 public class FinanceService {
@@ -22,8 +26,11 @@ public class FinanceService {
     private final CompanyRepository companyRepository;
     private final DividendRepository dividendRepository;
 
+    // 요청이 자주 들어오는가?
+    // 자주 변경되는 데이터 인가?
+    @Cacheable(key = "#companyName", value = CacheKey.KEY_FINANCE)
     public ScrapedResult getDividendByCompanyName(String companyName) {
-
+        log.info("search company -> " + companyName);
         // 1. 회사명 기준으로 회사 정보를 조회
         CompanyEntity company = this.companyRepository.findByName(companyName)
                 .orElseThrow(() -> new RuntimeException("존재하지 않는 회사명입니다."));
@@ -41,16 +48,10 @@ public class FinanceService {
 //        }
 
         List<Dividend> dividends = dividendEntities.stream()
-                                                    .map(e -> Dividend.builder()
-                                                            .date(e.getDate())
-                                                            .dividend(e.getDividend())
-                                                            .build())
-                                                    .collect((Collectors.toList()));
+                .map(e -> new Dividend(e.getDate(), e.getDividend()))
+                .collect((Collectors.toList()));
 
-        return new ScrapedResult(Company.builder()
-                                        .ticker(company.getTicker())
-                                        .name(company.getName())
-                                        .build(),
+        return new ScrapedResult(new Company(company.getTicker(), company.getName()),
                 dividends);
     }
 
